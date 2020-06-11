b471ecc1db4e655e838d736950e6b7903a447a31
Jeremy Weiss <j.weiss@yale.edu>
2020-06-11 19:11:32 -0400

    Add basic Google OAuth functionality.

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -16,107 +16,94 @@ package com.google.sps.servlets;
 
 import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.auth.oauth2.*;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.oauth2.Oauth2;
-import com.google.api.services.oauth2.model.Tokeninfo;
-import com.google.api.services.oauth2.model.Userinfoplus;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.GenericUrl;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.InputStream;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
-   /**
-   * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
-   * globally shared instance across your application.
-   */
-  //  private static FileDataStoreFactory dataStoreFactory;
-  // private DataStoreFactory datastoreFactory = DatastoreServiceFactory.getDatastoreService();
-  private static final AppEngineDataStoreFactory DATA_STORE_FACTORY =
-        AppEngineDataStoreFactory.getDefaultInstance();
-
-
-  /** Global instance of the HTTP transport. */
+  private static AppEngineDataStoreFactory DATA_STORE_FACTORY = AppEngineDataStoreFactory.getDefaultInstance();
   private static HttpTransport httpTransport;
-
-  /** Global instance of the JSON factory. */
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-  /** OAuth 2.0 scopes. */
-    private static final List<String> SCOPES = Arrays.asList(
-        "https://www.googleapis.com/auth/userinfo.email");
-
-  private static Oauth2 oauth2;
+  private static JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/userinfo.email");
   private static GoogleClientSecrets clientSecrets;
-
-  private static final String APPLICATION_NAME = "Portfolio/1.0";
-
-  /** Directory to store user credentials. */
-  private static final java.io.File DATA_STORE_DIR =
-      new java.io.File("store/oauth2_sample");
+  private static String APPLICATION_NAME = "Portfolio/1.0";
+  private static GoogleAuthorizationCodeFlow flow;
+  private static String redirectUri;
+  private static Gson gson = new Gson();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     try {
       httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-      // dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-      // authorization
-      Credential credential = authorize();
-      // set up global Oauth2 instance
-      oauth2 = new Oauth2.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(
-          APPLICATION_NAME).build();
-      // run commands
-      System.out.println("Validating a token");
-      Tokeninfo tokeninfo = oauth2.tokeninfo().setAccessToken(credential.getAccessToken()).execute();
-      System.out.println("BEFORE");
-      System.out.println(tokeninfo.toPrettyString());
-      if (!tokeninfo.getAudience().equals(clientSecrets.getDetails().getClientId())) {
-        System.err.println("ERROR: audience does not match our client ID!");
-      }
-      System.out.println("AFTER");
-
-      System.out.println("Obtaining User Profile Information");
-      Userinfoplus userinfo = oauth2.userinfo().get().execute();
-      System.out.println(userinfo.toPrettyString());
-      // success!
-      System.out.println("END");
+      clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+                       new InputStreamReader(LoginServlet.class.getResourceAsStream("/client.json")));
+      flow = new GoogleAuthorizationCodeFlow.Builder(
+                       httpTransport, JSON_FACTORY, clientSecrets, SCOPES).setDataStoreFactory(
+                       DATA_STORE_FACTORY).build();
+    } catch (Exception e) {
+      System.out.println(e);
       return;
-    } catch (IOException e) {
-    System.out.println("ERROR1");
-      System.err.println(e.getMessage());
-    } catch (Throwable t) {
-    System.out.println("ERROR2");
-      t.printStackTrace();
+    }
+
+    String code = request.getParameter("code"); // Todo unique 'value' parameter as protection
+    if (code != null) {
+      // Successful login
+      TokenResponse token = flow.newTokenRequest(code).setRedirectUri("http://localhost:8080/login").execute();
+      System.out.println(token);
+      // Credential credential = flow.createAndStoreCredential(token, <unique id>);
+      Credential credential = new Credential(BearerToken.authorizationHeaderAccessMethod()).setFromTokenResponse(token);
+      System.out.println(credential);
+
+      String emailUrl = "https://openidconnect.googleapis.com/v1/userinfo";
+      GenericUrl authUrl = new GenericUrl(emailUrl);
+      HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential);
+      HttpResponse authResponse = requestFactory.buildGetRequest(authUrl).execute();
+
+      HashMap<String, String> authContent = gson.fromJson(streamToString(authResponse.getContent()), HashMap.class);
+      String email = authContent.get("email");
+
+      if (email.equals(System.getenv("email"))) {
+        System.out.println("Welcome, Jeremy!");
+      } else {
+        System.out.println("Who're you???");
+      }
+
+    } else {
+      // Redirect to Google oauth
+      String redirectUri = flow.newAuthorizationUrl().setRedirectUri("http://localhost:8080/login").build();
+      response.sendRedirect(redirectUri);
     }
   }
 
-  private static Credential authorize() throws Exception {
-      // load client secrets
-      clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-          new InputStreamReader(LoginServlet.class.getResourceAsStream("/client.json")));
-      // set up authorization code flow
-      GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-          httpTransport, JSON_FACTORY, clientSecrets, SCOPES).setDataStoreFactory(
-          DATA_STORE_FACTORY).build();
-      // authorize
-      return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+  private String streamToString(InputStream stream) {
+    Scanner s = new Scanner(stream).useDelimiter("\\A");
+    String result = s.hasNext() ? s.next() : "";
+    return result;
   }
 }
