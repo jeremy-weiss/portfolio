b48fcde857b42182a669633542358887827c8fc7
jeremy-weiss <j.weiss@yale.edu>
2020-06-15 18:04:34 -0400

    Implement FindMeetingQuery.query()

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,64 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> attendees = request.getAttendees();
+
+    // Find times that don't work
+    ArrayList<TimeRange> badRanges = new ArrayList<>();;
+    for (Event event : events) {
+      Collection<String> eventAttendees = event.getAttendees();
+      for (String attendee : attendees) {
+        if (eventAttendees.contains(attendee)) {
+          badRanges.add(event.getWhen());
+          break;
+        }
+      }
+    }
+    badRanges.sort(TimeRange.ORDER_BY_START);
+
+    // Prune time ranges
+    for (int i = 0; i < badRanges.size() - 1; i++) {
+      TimeRange range1 = badRanges.get(i);
+      TimeRange range2 = badRanges.get(i + 1);
+      // Range2 can contain range1 if they have equal start times, but that case is equivalent to overlap
+      if (range1.overlaps(range2)) {
+        if (range1.contains(range2)) {
+          // Irrelevant time range
+          badRanges.set(i+1, null);
+        } else {
+          // Merge overlapping
+          TimeRange combined = TimeRange.fromStartEnd(range1.start(), range2.end(), false);
+          badRanges.set(i, combined);
+        }
+      }
+    }
+
+    ArrayList<TimeRange> validRanges = new ArrayList<>();
+    long duration = request.getDuration();
+    int startTime = TimeRange.START_OF_DAY;
+    int endTime;
+    for (TimeRange range : badRanges) {
+      if (range == null) {
+        continue;
+      }
+      endTime = range.start();
+      addIfAvailable(validRanges, startTime, endTime, duration);
+      startTime = range.end();
+    }
+    addIfAvailable(validRanges, startTime, TimeRange.END_OF_DAY + 1, duration);
+
+    return validRanges;
+  }
+
+  // Adds a time range to the collection if a meeting can be held in the specified time
+  private void addIfAvailable(Collection<TimeRange> validRanges, int startTime, int endTime, long duration) {
+    if (endTime - startTime >= duration) {
+      TimeRange validRange = TimeRange.fromStartEnd(startTime, endTime, false);
+      validRanges.add(validRange);
+    }
   }
 }
